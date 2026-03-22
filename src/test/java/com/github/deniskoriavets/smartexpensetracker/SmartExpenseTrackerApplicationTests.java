2135b9852eb124cb1ac538eda7050bcc8c985559
DenisKoriavets <d.korjavets@ukr.net>
2026-03-22 22:08:34 +0200

    test: add active profile test to context load test

--- src/test/java/com/github/deniskoriavets/smartexpensetracker/SmartExpenseTrackerApplicationTests.java
+++ src/test/java/com/github/deniskoriavets/smartexpensetracker/SmartExpenseTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.deniskoriavets.smartexpensetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartExpenseTrackerApplicationTests {
 
     @Test
